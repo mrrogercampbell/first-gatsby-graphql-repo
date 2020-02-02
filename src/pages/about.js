653a9cc8ba503963c30c1b440d12ebce9c1ed95b
Roger Campbell II <rc1336@gmail.com>
2020-02-02 18:57:10 -0500

    Added in logic for first page query

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,20 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-
-export default () => (
+export default ({ data }) => (
     <Layout>
-        <h1>Lets Talk About How Sci-Fi is awesome!</h1>
+        <h1>Lets Talk About How {data.site.siteMetadata.title}</h1>
         <p>
             Sci-Fi is the best genre ever! And this site is going to tell you all about it! "Engage!"
     </p>
     </Layout>
 )
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
