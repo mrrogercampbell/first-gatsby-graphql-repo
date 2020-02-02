06243d47de0c7b34eb1ec565dd6d5f6c2eaa0495
Roger Campbell II <rc1336@gmail.com>
2020-02-02 18:37:33 -0500

    Created starter template.

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>Lets Talk About How Sci-Fi is awesome!</h1>
+        <p>
+            Sci-Fi is the best genre ever! And this site is going to tell you all about it! "Engage!"
+    </p>
+    </Layout>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Sci-Fi is awesome!</h1>
+    <div>
+      <img
+        src="https://cdn.wallpapersafari.com/65/89/JXRhgV.jpg"
+        alt="Mr. Futuristic Sci-Fi"
+        height="500px"
+      />
+    </div>
+  </Layout>
+)
